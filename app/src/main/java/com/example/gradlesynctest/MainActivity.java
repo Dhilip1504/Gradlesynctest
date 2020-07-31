74c548cbd82db122dad4475c51a1745493ed15f3
Dhilip1504 <dhilip1504@gmail.com>
2020-07-31 13:10:29 +0530

    dhilip

--- app/src/main/java/com/example/gradlesynctest/MainActivity.java
+++ app/src/main/java/com/example/gradlesynctest/MainActivity.java
@@ -11,9 +11,11 @@ import android.view.MenuItem;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
     public Toolbar toolbar;
     private ViewPager myViewPager;
     private TabLayout tabLayout;
@@ -25,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
 
         myViewPager = (ViewPager) findViewById(R.id.myViewPager);
         tabAccessorAdapter = new TabAccessorAdapter(getSupportFragmentManager(), 0);
